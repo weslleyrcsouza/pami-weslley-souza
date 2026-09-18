5bd7c20a99cd805207201f33727e2280bc9ba7cd
Weslley Romã Campos de Souza <2311weslleysouza@gmail.com>
2026-09-17 23:57:06 -0300

    Delete aula03/aula-typescript directory

--- aula03/aula-typescript/exercicios-ts/exercicio2.ts
+++
@@ -1,7 +0,0 @@
-let cidades: string[] = ['São Paulo', 'Rio de Janeiro', 'Curitiba', 'Salvador', 'Recife'];
-
-function listarCidades(cidades: string[]): void {
-    cidades.forEach(cidade => console.log(cidade));
-}
-
-listarCidades(cidades);
